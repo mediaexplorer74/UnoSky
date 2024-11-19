2c36e137f0099ad49f17d6073e032cfa415c11c8
Martin Zikmund <martin@zikmund.dev>
2024-11-19 09:50:43 +0100

    Initial layout

---
+++ Birdsky.App/Birdsky.App/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Birdsky.App;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+    }
+}
---
+++ Birdsky.App/Birdsky.App/Platforms/WebAssembly/Program.cs
@@ -0,0 +1,13 @@
+namespace Birdsky.App;
+
+public class Program
+{
+    private static App? _app;
+
+    public static int Main(string[] args)
+    {
+        Microsoft.UI.Xaml.Application.Start(_ => _app = new App());
+
+        return 0;
+    }
+}
